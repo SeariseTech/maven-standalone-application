5da79f8f1f3633a4f084f0a8848e23c4c3ad4952
Searise Technology Solutions <122478567+SeariseTech@users.noreply.github.com>
2023-11-20 20:41:31 -0500

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Engineers, Welcome to Searise Technologies DevOps Master Class. You are sudying Maven Build Tool. !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello Engineers, Welcome to Searise Technologies DevOps Master Class. You are sudying Maven Build Tool. I have With me today in class, Engineer Ope and Engineer Glory !! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
